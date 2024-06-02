aa312023eb2e20a32b862d20231abc3cc1153685
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2024-06-02 15:40:49 +0700

    feat: add reactrouter and tailwindcss

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import '@fontsource/inter'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './index.css'
+import { Root } from './route'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
---
+++ src/route/root.jsx
@@ -0,0 +1,5 @@
+export default function Root() {
+  return (
+    <h1 className='text-3xl font-medium'>Balancify</h1>
+  )
+}
